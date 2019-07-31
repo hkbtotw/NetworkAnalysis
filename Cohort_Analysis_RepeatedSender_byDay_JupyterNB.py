b7430968ecd8ed2ab6114bf365252769e45ea636
hkbtotw <hkbtotw@gmail.com>
2019-07-31 22:57:27 +0700

    Cohort Analysis - Retention by day - Fixed blank date

--- Cohort_Analysis_RepeatedSender_byDay_JupyterNB.py
+++ Cohort_Analysis_RepeatedSender_byDay_JupyterNB.py
@@ -9,11 +9,13 @@ from datetime import datetime
 pd.set_option('max_columns',50)
 mpl.rcParams['lines.linewidth']=2
 
-%matplotlib inline
+#%matplotlib inline
 
-df1=pd.read_excel('TBPoint_Transaction_TC.xlsx',  sheet_name='Transaction_UA')
+df1=pd.read_excel(r'C:\Users\70018928\Documents\Project 2019\Ad-hoc\Point System\TBPoint_Transaction_TC.xlsx',  sheet_name='Transaction_UB')
+
+
 df= df1[['Id','L_Sender','Amount', 'ProjectId', 'Timestamp']].copy()
-df['Timestamp'] =  pd.to_datetime(df['Timestamp'], format="%Y-%m-%d %H:%M:%S")
+df['Timestamp'] =  pd.to_datetime(df['Timestamp'], format="%Y-%m-%d")
 daylist=[]
 for n in df['Timestamp'].tolist():
     daylist.append(datetime.strftime(n,'%d'))
@@ -23,12 +25,20 @@ df_dummy.columns=['day_no']
 
 df=pd.concat([df,df_dummy['day_no']],axis=1)
 
+
+
 df.set_index('L_Sender', inplace=True)
 
 
 df['CohortGroup'] = df.groupby(level=0)['day_no'].min()
 df.reset_index(inplace=True)
-grouped = df.groupby(['CohortGroup', 'day_no'])
+
+df['CohortPeriod']=df['day_no'].astype(int)-df['CohortGroup'].astype(int)+1
+
+
+print(df)
+
+grouped = df.groupby(['CohortGroup', 'day_no', 'CohortPeriod'])
 
 
 # count the unique users, orders, and total revenue per Group + Period
@@ -37,25 +47,10 @@ cohorts = grouped.agg({'L_Sender': pd.Series.nunique})
 # make the column names more meaningful
 cohorts.rename(columns={'L_Sender': 'No_Sender'}, inplace=True)
 
-#cohorts.tail()
-#df.head()
-def cohort_period(df):
-    """
-    Creates a `CohortPeriod` column, which is the Nth period based on the user's first purchase.
-    
-    Example
-    -------
-    Say you want to get the 3rd month for every user:
-        df.sort(['UserId', 'OrderTime', inplace=True)
-        df = df.groupby('UserId').apply(cohort_period)
-        df[df.CohortPeriod == 3]
-    """
-    df['CohortPeriod'] = np.arange(len(df)) + 1
-    return df
 
-cohorts = cohorts.groupby(level=0).apply(cohort_period)
 
-# reindex the DataFrame
+
+#print(cohorts)
 cohorts.reset_index(inplace=True)
 cohorts.set_index(['CohortGroup', 'CohortPeriod'], inplace=True)
 
@@ -87,10 +82,10 @@ plt.legend(loc='best', fontsize = 'small')
 import seaborn as sns
 sns.set(style='white')
 
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(14, 10))
 plt.title('Cohorts: Repeated Sender')
 
-yticklabels = range(1, 17, 1)
+yticklabels = range(1, 22, 1)
 # the index position of the tick labels
 yticks = []
 count=1
@@ -99,7 +94,16 @@ for label in yticklabels:
     count=count+1
     yticks.append(idx_pos)
 
+xticklabels = range(1, 22, 1)
+# the index position of the tick labels
+xticks = []
+count=1
+for label in xticklabels:
+    idx_pos = count
+    count=count+1
+    xticks.append(idx_pos)
 
-ax1=sns.heatmap(user_retention.T, mask=user_retention.T.isnull(), annot=True, fmt='.0%',yticklabels=yticklabels, cbar=True,cbar_kws={"shrink": .82})
+ax1=sns.heatmap(user_retention.T, mask=user_retention.T.isnull(), annot=True, fmt='.0%',xticklabels=xticklabels,yticklabels=yticklabels, cbar=True,cbar_kws={"shrink": .82})
 ax1.set_yticks(yticks,  "center")
 plt.show();
+
